598cb5b2ca530b7742f4e7ad75055033f68a36f1
Xabier A <58989860+reroll7@users.noreply.github.com>
2020-09-16 11:57:52 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 source /etc/apache2/envvars
+echo "<?php echo 'Hello world! Container running with custom router (internal). Documentroot mounted on cephfs volume.';" > /mnt/app/index.php
 #tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
